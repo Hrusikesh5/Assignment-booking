b4ac41a77896b26f22039a3a66cbba517b76f204
drivadosayan <techsupport1@drivado.com>
2024-12-10 14:14:05 +0530

    added to the new code here

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./connection/db");
+// const connectDB = require("./connection/db");
 
 const CarRouter = require("./routes/CarRoutes");
 const bookingRouter = require("./routes/BookingRoutes");
 
-connectDB();
+// connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 // app.use(bodyParser.json());
 
 // app.use('/api/users', require('./routes/userRoutes'));
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, Developer!");
+});
 app.use("/api", CarRouter);
 app.use("/api", bookingRouter);
 
